383fb487b760829e470f6d6fe78220fc0f835bd9
Dustin <redhotblood1010@gmail.com>
2026-02-24 11:47:41 -0500

    yes

--- weLearnin.playground/Contents.swift
+++ weLearnin.playground/Contents.swift
@@ -26,4 +26,55 @@ func add(a: Int, b: Int)
 }
 
 add(a: 1, b: 5)
+//Challenge Problems
+func greet(name: String)
+{
+    print("Hello " + name)
+}
+greet(name: "Dustin")
 
+func max2(_ a: Int, _ b: Int) -> Int
+{
+    return a>b ? a:b
+}
+func firstChar(of s: String) -> String
+{
+    if let ch = s.first
+    {
+        return String(ch)
+    }
+    else
+    {
+        return "-"
+    }
+}
+
+let a = firstChar(of: "Hello World")
+let b = firstChar(of: "")
+print(a)
+print(b)
+
+func safeDivide(_ num: Int, by num2: Int) -> Int?
+{
+    if num2 == 0
+    {
+        return nil
+    }
+    else
+    {
+        return num/num2
+    }
+}
+
+if let c = safeDivide(10, by: 0)
+{
+    print(c)
+}
+else
+{
+    print("Cannot divide by zero")
+}
+if let d = safeDivide(12, by: 2)
+{
+    print(d)
+}
